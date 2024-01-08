c485add187e2026af8269790c898a2ea5ea951c0
Sugith <sugithkodappurath@gmail.com>
2024-01-08 23:03:19 +0530

    tawk fix

--- script1.js
+++ script1.js
@@ -66,21 +66,21 @@ function showProjects(projects) {
     projectsContainer.innerHTML = projectsHTML;
 
     // vanilla tilt.js
-    VanillaTilt.init(document.querySelectorAll(".tilt"), {
-        max: 20,
-    });
+    // VanillaTilt.init(document.querySelectorAll(".tilt"), {
+    //     max: 20,
+    // });
     // // vanilla tilt.js  
 
     // /* ===== SCROLL REVEAL ANIMATION ===== */
-    const srtop = ScrollReveal({
-        origin: 'bottom',
-        distance: '80px',
-        duration: 1000,
-        reset: true
-    });
+    // const srtop = ScrollReveal({
+    //     origin: 'bottom',
+    //     distance: '80px',
+    //     duration: 1000,
+    //     reset: true
+    // });
 
-    /* SCROLL PROJECTS */
-    srtop.reveal('.work .box', { interval: 200 });
+    // /* SCROLL PROJECTS */
+    // srtop.reveal('.work .box', { interval: 200 });
 
     // isotope filter products
     var $grid = $('.box-container').isotope({
@@ -110,7 +110,7 @@ var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
 (function () {
     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
     s1.async = true;
-    s1.src='https://embed.tawk.to/6589d6f607843602b805768d/1hih6vd9j';
+    s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
     s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
     s0.parentNode.insertBefore(s1, s0);
