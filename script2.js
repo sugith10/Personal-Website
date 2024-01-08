ec6971bcc666db697706e6d388bf670af9492ba5
Sugith <sugithkodappurath@gmail.com>
2024-01-08 22:58:33 +0530

    tawk fix

--- script2.js
+++ script2.js
@@ -35,7 +35,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
+s1.src='https://tawk.to/chat/6589d6f607843602b805768d/1hih6vd9j';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
@@ -65,7 +65,7 @@ document.onkeydown = function(e) {
 document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
-        document.title = "Experience | Dhananjay";
+        document.title = "Experience | Sugith";
         $("#favicon").attr("href","/assets/images/favicon.png");
     }
     else {
